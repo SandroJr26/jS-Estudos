559577b5b5beec155cfbab0d79afb0a4a3458aee
Sandro Junior <nunet26@gmail.com>
2023-03-19 05:32:22 -0300

    Update script.js

--- promises/script.js
+++ promises/script.js
@@ -4,18 +4,18 @@
   
   ASSINCRONA
   EX: BANCO PEGAR CEP
-  CEP -> Correireios
+  CEP -> Correios
 
   PROMISES é um objeto JS que te dá um conjunto de funcionalidades pra gerenciar um controle melhor da parte assincrona da linguagem
   */
 
-console.log("Primeiro");
+// console.log("Primeiro");
 
 setTimeout(function () {
-  console.log("Segundo");
+  // console.log("Segundo");
 }, 0);
 
-console.log("Terceiro");
+// console.log("Terceiro");
 
 /*
   PENDENTE
@@ -29,7 +29,7 @@ function alreadyPerformed() {
 }
 
 alreadyPerformed().then(function (data) {
-  console.log("Promise dasd", data);
+  // console.log("Promise dasd", data);
 });
 
 function alreadyRejected() {
@@ -37,7 +37,7 @@ function alreadyRejected() {
 }
 
 alreadyRejected().catch(function () {
-  // console.log("Promise rejeitada");
+  console.log("Promise rejeitada");
 });
 
 // Maneira de uso para um melhor gerenciamento de código
@@ -51,27 +51,72 @@ function promiseWorked() {
 }
 
 promiseWorked().then(function (message) {
-  console.log(message);
+  // console.log(message);
 });
 
 promiseWorked().catch(function (message) {
-  console.log(message);
+  // console.log(message);
 });
 
 // https://viacep.com.br/ws/46430000/json/
 
-alreadyPerformed().then(function (cep) {
-  return fetch("https://viacep.com.br/ws/" + cep + "/json/")
-    .then(function (data) {
-      return data.json();
-    })
-    .then(function () {
-      return alreadyRejected();
-    })
-    .then(function (address) {
-      console.log(address.cep);
-    })
-    .catch(function () {
-      console.log("Aconteceu algum erro");
-    });
-});
+function getCEP(cep) {
+  return fetch("https://viacep.com.br/ws/" + cep + "/json/");
+}
+function getData(data) {
+  return data.json();
+}
+function reject() {
+  return alreadyRejected();
+}
+function getAddress(address) {
+  // console.log(address);
+}
+function handleError(error) {
+  // console.log("Aconteceu algum erro", error);
+}
+
+alreadyPerformed()
+  .then(getCEP)
+  .then(getData)
+  .then(reject)
+  .then(getAddress)
+  .catch(handleError)
+  .finally(function () {
+    // console.log("Sempre será executada");
+  });
+
+function delay500() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve("Demora 500");
+    }, 500);
+    // reject("Foi recusada");
+  });
+}
+
+function delay200() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve("Demora 200");
+    }, 200);
+    // reject("Foi recusada");
+  });
+}
+
+// Retorna todas as Promises após serem resolvidas
+// Promise.all([delay200(), alreadyPerformed(), delay500("46430000")]).then(
+//   function (resolvidPromisse) {
+//     console.log(resolvidPromisse);
+//   }
+// );
+
+// Retorna a primeira Promisse resolvida ou primeira rejeitada
+Promise.race([delay200(), alreadyRejected(), delay500("46430000")]).then(
+  function (resolvidPromisse) {
+    console.log(resolvidPromisse);
+  },
+  function (valueRejected) {
+    console.log(valueRejected);
+  }
+);
