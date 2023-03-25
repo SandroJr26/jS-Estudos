4bfd05fb681ae8fa88143ffe70ccc81e7716266b
Sandro Junior <nunet26@gmail.com>
2023-03-25 16:55:39 -0300

    error

--- node-js/index.js
+++ node-js/index.js
@@ -1,8 +1,16 @@
-const { Person } = require("./person");
+// const { Person } = require("./person");
+const dotenv = require("dotenv");
 
-// require("./modules/path");
-// require("./modules/fs");
-// require("./modules/http");
-require("./modules/express");
+const connectToDatabase = require("./src/database/connect");
 
-const person = new Person("Sandro");
+dotenv.config();
+
+connectToDatabase();
+
+// // require("./modules/path");
+// // require("./modules/fs");
+// // require("./modules/http");
+
+// require("./modules/express");
+
+// const person = new Person("Sandro");
