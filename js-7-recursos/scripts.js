a216523b86e8f068f098ab126e890021b24f2523
Sandro Junior <nunet26@gmail.com>
2023-03-28 02:55:32 -0300

    base do JS

--- js-7-recursos/scripts.js
+++ js-7-recursos/scripts.js
@@ -44,4 +44,135 @@ const numbersPairs = numbers.filter((number) => {
   return number % 2 === 0;
 });
 
-console.log(numbersPairs);
+// 3 - Destructuring
+const users = {
+  name: "Sandro",
+  age: 26,
+  email: "sandrojkl26@hotmail.com",
+};
+
+// extrair nome e email
+const { name, email } = users;
+// console.log(name, email);
+
+// extrair itens de array
+const numbersB = [1, 2, 3];
+const [a, b, c] = numbersB;
+
+// console.log(a, b, c);
+
+function displayDataUsers({ name, email }) {
+  console.log("Nome: " + name);
+  console.log("Email: " + email);
+}
+
+// displayDataUsers(users);
+
+// 4 - Template literals
+const nameUser = "Sandro";
+const age = 26;
+const message = `${nameUser} tem ${age} anos de idade.`;
+
+message;
+
+const balance = 1000;
+const messageB = `O seu saldo atual é de: ${
+  balance > 0 ? `R$ ${balance}` : "negativo"
+}`;
+
+messageB;
+
+// 5 - Rest e spread
+// rest
+function sumC(...numbers) {
+  return numbers.reduce((total, number) => {
+    return total + number;
+  });
+}
+sumC(1, 2, 3, 4, 5);
+
+// spread
+const numbers1 = [1, 2, 3];
+const numbers2 = [4, 5, 6];
+const combinedNumbers = [...numbers1, ...numbers2];
+
+combinedNumbers;
+
+const usersB = {
+  name: "Sandro",
+  age: 26,
+};
+
+const userWithEmail = { ...usersB, address: "Rua Tomaz " };
+
+userWithEmail;
+
+// 6 - Classes
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  present() {
+    console.log(`O meu nome é ${this.name} e eu tenho ${this.age}`);
+  }
+}
+
+const sandro = new Person("Sandro", 26);
+const ricardo = new Person("Ricardo", 22);
+
+// sandro.present();
+// ricardo.present();
+
+class Employee extends Person {
+  constructor(name, age, wage) {
+    super(name, age);
+    this.wage = wage;
+  }
+  presentEmployee() {
+    // console.log(
+    //   `O meu nome é ${this.name} recebo ${this.wage} e tenho ${this.age}`
+    // );
+  }
+}
+
+const wageEmployee = new Employee("Sandro", 26, 4500);
+
+wageEmployee.presentEmployee();
+
+// 7 - Métodos de Array
+// map
+const numbersC = [1, 2, 3, 4];
+const doubleNumbers = numbersC.map((number) => {
+  return number * 2;
+});
+
+doubleNumbers;
+
+// filter
+const numbersD = [1, 2, 3, 4, 5, 6, 7];
+const numbersOdd = numbersD.filter((number) => {
+  return number % 2 === 1;
+});
+
+numbersOdd;
+
+// reduce
+const numbersE = [1, 2, 3, 4, 5, 6, 7];
+const sumNumbers = numbersE.reduce((total, number) => {
+  return total + number;
+}, 0);
+sumNumbers;
+
+// find
+const numbersF = [1, 2, 3, 4, 5, 6, 7];
+const number3 = numbersF.find((number) => {
+  return number === 3;
+});
+number3;
+
+// forEach
+const names = ["Sandro", "Rafael", "Ricardo"];
+names.forEach((name) => {
+  // console.log(name);
+});
